3cd5642e22e1c97fe082f1184fcdfd1fd9bdef20
BeatrizCerqueira <beatriz_cbj@hotmail.com>
2025-05-20 23:03:34 -0300

    consultas requisito 3

---
+++ sql/consultas/avancadas/A5_carga_horaria_professor.sql
@@ -0,0 +1,11 @@
+-- Requisito 2.4: Consultar carga horária atual de um professor
+SELECT 
+    p.NO_PROFESSOR as professor,
+    COALESCE(COUNT(DISTINCT t.ID_HORARIO) * 2, 0) as carga_horaria
+FROM 
+    TB_PROFESSOR p
+    LEFT JOIN TB_TURMA t ON p.ID_PROFESSOR = t.ID_PROFESSOR
+GROUP BY 
+    p.NO_PROFESSOR
+ORDER BY 
+    p.NO_PROFESSOR; 
